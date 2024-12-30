ea88b93f50987e58de2a94894af6c9d8ba1b63c0
luc <luc@forgotten.name>
2024-12-29 21:09:46 -0500

    .

--- docker/flake.nix
+++ docker/flake.nix
@@ -1,29 +1,47 @@
-# docker/flake.nix
 {
   description = "Nix flake to build a NixOS-based Docker image";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";  # or your target system
-    pkgs = nixpkgs.legacyPackages.${system};
+    system = "aarch64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      config.allowUnsupportedSystem = true;
+    };
   in {
     dockerImage = pkgs.dockerTools.buildImage {
       name = "dreamshell-nixos";
       tag = "latest";
-      contents = [
-        pkgs.bash
-        pkgs.coreutils
-        pkgs.docker
-        # Add other necessary packages here
-      ];
+
+      copyToRoot = pkgs.buildEnv {
+        name = "image-root";
+        paths = [
+          pkgs.bash
+          pkgs.coreutils
+          pkgs.docker
+          pkgs.cowsay
+        ];
+        pathsToLink = [ "/bin" ];
+      };
+
+      runAsRoot = ''
+        #!${pkgs.runtimeShell}
+        mkdir -p /data
+      '';
+
       config = {
-        Cmd = [ "/bin/bash" ];
+        Cmd = [ "/bin/bash" "-c" "cowsay hello" ];
+        WorkingDir = "/data";
         ExposedPorts = {
           "22/tcp" = {};
           "80/tcp" = {};
         };
       };
+
+      diskSize = 1024;
+      buildVMMemorySize = 512;
     };
   };
 }
